6ad2388346d76c318f0a61cd7c459a0d88e31474
Captian <57861328+Cptdaastan@users.noreply.github.com>
2020-07-26 14:00:38 +0530

    Add files via upload
    This just for git test information of sight

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Calculator:
+    def __init__(self):
+        self.current =0
+        self.total =0
+    def set(self,value):
+        self.current = value
+        
+    def add(self):
+        self.total+=self.current
+    def sub(self):
+        self.total-=self.current
+    def total(self):
+        return self.total
+
+
+# In[ ]:
+
+
+
+
